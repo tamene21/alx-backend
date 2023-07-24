8cb5ab8ae81f51949913daf9461d4accf2884cff
Tamene Gelaye <tamenes21@gmail.com>
2023-07-24 19:08:44 +0000

    pagination index

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Simple pagination helper function'''
+from typing import tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    Retrive starting and ending index
+    '''
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return(start_index, end_index)
