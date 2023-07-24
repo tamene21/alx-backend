63f5aeea92e8de34168f924a3b93c097bc292f6a
Tamene Gelaye <tamenes21@gmail.com>
2023-07-24 19:23:19 +0000

    correction

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-'''Simple pagination helper function'''
+"""Simple pagination helper function"""
 from typing import tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    '''
-    Retrive starting and ending index
-    '''
+    """ Retrive starting and ending index"""
     start = (page - 1) * page_size
     end = start + page_size
-    return(start, end)
+    return (start, end)
