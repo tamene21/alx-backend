1154ecf1c16078144d2ebbcdccc0338479a7d8ee
Tamene Gelaye <tamenes21@gmail.com>
2023-07-24 19:11:44 +0000

    edited

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,5 +8,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Retrive starting and ending index
     '''
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = start_index + page_size
     return(start_index, end_index)
