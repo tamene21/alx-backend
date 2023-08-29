d81f7427242fdb8ce7827109bc03dd17fe77889e
tamene gelaye <tamenes21@gmail.com>
2023-08-29 10:31:59 +0000

    syntax correction

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -24,4 +24,4 @@ async function displaySchoolValue(schoolName) {
 
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');i
+displaySchoolValue('HolbertonSanFrancisco');
