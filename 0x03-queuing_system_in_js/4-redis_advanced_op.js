ee3aa00b571150e75f7e1c6a6c684ecfc7603729
tamene gelaye <tamenes21@gmail.com>
2023-08-29 11:03:06 +0000

    variable

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -9,7 +9,7 @@ client.on('connect',() => console.log('Redis client connected to the server'));
 //checking sever connection and if it fail throw error message
 client.on('error', (err) => console.log(`Redis client not connected to the server:${err.message}`));
 
-const KEY = HolbertonSchools;
+const KEY = 'HolbertonSchools';
 const keys = ['Portland', 'Seattle', 'New York', 'Bogota', 'Cali', 'Paris'];
 const values = [50, 80, 20, 20, 40, 2];
 
