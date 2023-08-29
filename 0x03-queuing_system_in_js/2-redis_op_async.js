c9eab9a01b68d7219d08f0c81cc26fb4a8812974
tamene gelaye <tamenes21@gmail.com>
2023-08-29 10:29:50 +0000

    async function

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,27 @@
+// Import redis
+
+import { createClient } from 'redis';
+import redis from 'redis';
+
+const { promisify } = require('util');
+
+const client = createClient();
+
+const asyncGet = promisify(client.get).bind(client);
+// checking server connection
+client.on('connect',() => console.log('Redis client connected to the server'));
+//checking sever connection and if it fail throw error message
+client.on('error', (err) => console.log(`Redis client not connected to the server:${err.message}`));
+
+function setNewSchool(schoolName, value){
+	// set a redis value and print redis message
+	client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+	console.log(await aysncGet(schoolName));
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');i
