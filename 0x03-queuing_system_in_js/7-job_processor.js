3862156324d60e7d28a0cd1346d6c23946f955ad
tamene gelaye <tamenes21@gmail.com>
2023-08-30 15:45:14 +0000

    checking job progress

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,28 @@
+import {createQueue} from 'kue';
+
+const blackList = ['4153518780', '4153518781];
+const sendNotification = (phoneNumber, message, job, done) => {
+	const total = 100;
+
+	job.progress(0, total);
+
+	if (blackList.includes(phoneNumber)) {
+	    done(Error(`Phone number ${phoneNumber}is blacklisted`));
+	    return;
+	};
+
+	job.progress(50, total);
+	console.log(
+	    `Sending notification to ${phoneNumber}, with message: ${message}`
+	);
+	done();
+}
+
+const queue = createQueue();
+const queueName = 'push_notification_code_2';
+
+queue.process(queueName, 2, (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumbe, message, job, done);
+})
+
